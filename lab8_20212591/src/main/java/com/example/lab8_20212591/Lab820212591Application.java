fa9736600164787d8be242e94f9a3c7248d6d209
Daniel <a20212591@pucp.edu.pe>
2024-11-01 23:18:25 -0500

    Lab8

---
+++ lab8_20212591/src/main/java/com/example/lab8_20212591/Lab820212591Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20212591;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820212591Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab820212591Application.class, args);
+	}
+
+}
